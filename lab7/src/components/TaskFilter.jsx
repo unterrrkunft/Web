829677fc7885eefbffb97d5fbd099bd344ea0169
unterrrkunft <114381125+unterrrkunft@users.noreply.github.com>
2026-04-13 07:08:35 +0300

    Add files via upload

---
+++ lab7/src/components/TaskFilter.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react'
+import { TaskContext } from '../App'
+
+export default function TaskFilter() {
+  const { filter, setFilter } = useContext(TaskContext)
+
+  return (
+    <div className="filters">
+      <button onClick={() => setFilter('all')} disabled={filter === 'all'}>
+        All
+      </button>
+
+      <button onClick={() => setFilter('active')} disabled={filter === 'active'}>
+        Active
+      </button>
+
+      <button onClick={() => setFilter('completed')} disabled={filter === 'completed'}>
+        Completed
+      </button>
+    </div>
+  )
+}
