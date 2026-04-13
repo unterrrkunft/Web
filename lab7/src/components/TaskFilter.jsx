5a25723031ef49e4946aa4e3192e89d5dd166243
unterrrkunft <114381125+unterrrkunft@users.noreply.github.com>
2026-04-13 07:42:43 +0300

    .

--- lab7/src/components/TaskFilter.jsx
+++ lab7/src/components/TaskFilter.jsx
@@ -1,20 +1,29 @@
 import { useContext } from 'react'
-import { TaskContext } from '../App'
+import { TaskContext } from '../context/TaskContext'
 
 export default function TaskFilter() {
   const { filter, setFilter } = useContext(TaskContext)
 
   return (
     <div className="filters">
-      <button onClick={() => setFilter('all')} disabled={filter === 'all'}>
+      <button
+        onClick={() => setFilter('all')}
+        disabled={filter === 'all'}
+      >
         All
       </button>
 
-      <button onClick={() => setFilter('active')} disabled={filter === 'active'}>
+      <button
+        onClick={() => setFilter('active')}
+        disabled={filter === 'active'}
+      >
         Active
       </button>
 
-      <button onClick={() => setFilter('completed')} disabled={filter === 'completed'}>
+      <button
+        onClick={() => setFilter('completed')}
+        disabled={filter === 'completed'}
+      >
         Completed
       </button>
     </div>
